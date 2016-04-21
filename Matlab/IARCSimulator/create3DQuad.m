b542930f3eea31051f3f2200ac5dbea4a6fd0503
Trexter <sheridak@purdue.edu>
2016-04-21 00:07:04 -0400

    Finished the draw3DQuad Function. This draws a 3d quad using lines. Really big pain in the ass.

--- Matlab/IARCSimulator/create3DQuad.m
+++ Matlab/IARCSimulator/create3DQuad.m
@@ -1,20 +1,21 @@
 function create3DQuad(quadcopter)
 %This will create the 3D quadrotor using plot3
 
-baseColor = 'b';
+baseColor = 'k';
 baseLineWidth = 3;
-armColor = 'b';
+armColor = 'r';
+armLineWidth = 5;
 
-hold on
 % create the base of the quad
+hold on
 %BOTTOM SIDE
 points = zeros(3, 2);
 points(1:3, 1) = [-0.07, -0.07, -0.04]';
 points(1:3, 2) = [0.07, -0.07, -0.04]';
 %rotate the points
-points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)')
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
 points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
-p = plot3(points(1, :), points(3, :), points(3, :), baseColor);
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
 p.LineWidth = baseLineWidth;
 
 %nextline
@@ -22,9 +23,9 @@ points = zeros(3, 2);
 points(1:3, 1) = [-0.07, 0.07, -0.04]';
 points(1:3, 2) = [0.07, 0.07, -0.04]';
 %rotate the points
-points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)')
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
 points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
-p = plot3(points(1, :), points(3, :), points(3, :), baseColor);
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
 p.LineWidth = baseLineWidth;
 
 %nextline
@@ -32,9 +33,9 @@ points = zeros(3, 2);
 points(1:3, 1) = [-0.07, -0.07, -0.04]';
 points(1:3, 2) = [-0.07, 0.07, -0.04]';
 %rotate the points
-points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)')
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
 points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
-p = plot3(points(1, :), points(3, :), points(3, :), baseColor);
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
 p.LineWidth = baseLineWidth;
 
 %nextline
@@ -42,10 +43,147 @@ points = zeros(3, 2);
 points(1:3, 1) = [0.07, -0.07, -0.04]';
 points(1:3, 2) = [0.07, 0.07, -0.04]';
 %rotate the points
-points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)')
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
 points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
-p = plot3(points(1, :), points(3, :), points(3, :), baseColor);
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
 p.LineWidth = baseLineWidth;
+
+%TOP SIDE
+points = zeros(3, 2);
+points(1:3, 1) = [-0.07, -0.07, 0.04]';
+points(1:3, 2) = [0.07, -0.07, 0.04]';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = baseLineWidth;
+
+%nextline
+points = zeros(3, 2);
+points(1:3, 1) = [-0.07, 0.07, 0.04]';
+points(1:3, 2) = [0.07, 0.07, 0.04]';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = baseLineWidth;
+
+%nextline
+points = zeros(3, 2);
+points(1:3, 1) = [-0.07, -0.07, 0.04]';
+points(1:3, 2) = [-0.07, 0.07, 0.04]';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = baseLineWidth;
+
+%nextline
+points = zeros(3, 2);
+points(1:3, 1) = [0.07, -0.07, 0.04]';
+points(1:3, 2) = [0.07, 0.07, 0.04]';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = baseLineWidth;
+
+
+%CREATE THE ARM 1
+drawArm([0.07, 0.07, 0], 0.27, 0.02, [1, 1, 0], quadcopter, armColor, baseColor, armLineWidth, baseLineWidth);
+drawArm([-0.07, 0.07, 0], 0.27, 0.02, [-1, 1, 0], quadcopter, armColor, baseColor, armLineWidth, baseLineWidth);
+drawArm([0.07, -0.07, 0], 0.27, 0.02, [1, -1, 0], quadcopter, armColor, baseColor, armLineWidth, baseLineWidth);
+drawArm([-0.07, -0.07, 0], 0.27, 0.02, [-1, -1, 0], quadcopter, armColor, baseColor, armLineWidth, baseLineWidth);
 
 end
 
+function drawArm(base, length, thickness, dir, quadcopter, armColor, baseColor, armLineWidth, baseLineWidth)
+sqrt2 = sqrt(2); %sqrt(2)
+
+points = zeros(3, 2);
+points(1:3, 1) = [base(1) + (thickness * dir(1) * -1), base(2), base(3) + thickness]';
+points(1:3, 2) = (points(1:3, 1) + [dir(1) * length / sqrt2; dir(2) * length / sqrt2; 0])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), armColor);
+p.LineWidth = armLineWidth;
+
+points(1:3, 1) = [base(1), base(2) + (thickness * dir(2) * -1), base(3) + thickness]';
+points(1:3, 2) = (points(1:3, 1) + [dir(1) * length / sqrt2; dir(2) * length / sqrt2; 0])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), armColor);
+p.LineWidth = armLineWidth;
+points(1:3, 1) = [base(1) + (thickness * dir(1) * -1), base(2), base(3) - thickness]';
+points(1:3, 2) = (points(1:3, 1) + [dir(1) * length / sqrt2; dir(2) * length / sqrt2; 0])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), armColor);
+p.LineWidth = armLineWidth;
+
+points(1:3, 1) = [base(1), base(2) + (thickness * dir(2) * -1), base(3) - thickness]';
+points(1:3, 2) = (points(1:3, 1) + [dir(1) * length / sqrt2; dir(2) * length / sqrt2; 0])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), armColor);
+p.LineWidth = armLineWidth;
+
+%draw base connects
+points(1:3, 1) = [base(1) + (thickness * dir(1) * -1), base(2), base(3) + 0.04]';
+points(1:3, 2) = (points(1:3, 1) + [0; 0; -0.08])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = armLineWidth;
+
+points(1:3, 1) = [base(1), base(2) + (thickness * dir(2) * -1), base(3) + 0.04]';
+points(1:3, 2) = (points(1:3, 1) + [0; 0; -0.08])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = armLineWidth;
+
+%draw end points
+points(1:3, 1) = [base(1) + (thickness * dir(1) * -1), base(2), base(3) + thickness]';
+points(1:3, 1) = (points(1:3, 1) + [dir(1) * length / sqrt2; dir(2) * length / sqrt2; 0])';
+points(1:3, 2) = (points(1:3, 1) + [(thickness * dir(1)); (thickness * dir(2)) * -1; 0])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = armLineWidth;
+
+points(1:3, 1) = [base(1) + (thickness * dir(1) * -1), base(2), base(3) - thickness]';
+points(1:3, 1) = (points(1:3, 1) + [dir(1) * length / sqrt2; dir(2) * length / sqrt2; 0])';
+points(1:3, 2) = (points(1:3, 1) + [(thickness * dir(1)); (thickness * dir(2)) * -1; 0])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = armLineWidth;
+
+points(1:3, 1) = [base(1) + (thickness * dir(1) * -1), base(2), base(3) - thickness]';
+points(1:3, 1) = (points(1:3, 1) + [dir(1) * length / sqrt2; dir(2) * length / sqrt2; 0])';
+points(1:3, 2) = (points(1:3, 1) + [0; 0; thickness * 2])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = armLineWidth;
+
+points(1:3, 1) = [base(1), base(2) + (thickness * dir(2) * -1), base(3) - thickness]';
+points(1:3, 1) = (points(1:3, 1) + [dir(1) * length / sqrt2; dir(2) * length / sqrt2; 0])';
+points(1:3, 2) = (points(1:3, 1) + [0; 0; thickness * 2])';
+%rotate the points
+points(1:3, 1) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 1)');
+points(1:3, 2) = quadcopter.pos + quatrotate(quadcopter.angleQuat, points(1:3, 2)');
+p = plot3(points(1, :), points(2, :), points(3, :), baseColor);
+p.LineWidth = armLineWidth;
+
+end
