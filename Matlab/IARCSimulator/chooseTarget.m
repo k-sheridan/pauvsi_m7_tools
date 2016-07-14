20fb5de13d29e7ec95bfc7fb03d292d0dc08ea2a
Trexter <sheridak@purdue.edu>
2016-07-14 15:10:03 -0400

    Started generating waypoints for trajectory generator

--- Matlab/IARCSimulator/chooseTarget.m
+++ Matlab/IARCSimulator/chooseTarget.m
@@ -1,7 +1,11 @@
-function [ output_args ] = chooseTarget( input_args )
+function [ targetIndex, numTaps ] = chooseTarget( roombas )
 %CHOOSETARGET Summary of this function goes here
-%   Detailed explanation goes here
+%   targetIndex: this is the index of the roomba vector. -1 = hover
+%   This will choose a target using a deep net
 
+%%TEMP
+targetIndex = rand(1, 9);
+numTaps = rand(0, 7);
 
 end
 
---
+++ Matlab/IARCSimulator/generatePath.m
@@ -0,0 +1,15 @@
+function [ trajectory, waypoints] = generatePath( targetPos, targetVel, quad )
+%GENERATEPATH Summary of this function goes here
+%   Generates a path from quad to roomba. It then will make an actuator constrained parmetric trajectory
+ROOMBA_PREDICT = 1.0;
+
+%%first predict the position of roomba when arriving
+dist = mag(targetPos - quad.pos)
+timeToArrival = ROOMBA_PREDICT * dist; % simple linear prediction
+%used the time to arrival and target state to predict the final goal
+%position
+goalPos = targetPos + (targetVel * timeToArrival); %this is the goal pos
+
+
+end
+
