15e4629e9504decc5e5d38ab26cf8ee6ed046cf3
Trexter <sheridak@purdue.edu>
2016-08-05 20:38:29 -0400

    fixed the simplification bug

--- Matlab/IARCSimulator/simplifyPath.m
+++ Matlab/IARCSimulator/simplifyPath.m
@@ -6,22 +6,22 @@ Waypoints(1, 1:3) = oldWaypoints(1, 1:3);
 
 %start the simplification loop from the second waypoint
 
-[m, ~] = size(oldWaypoints); % get the size of the waypoints
+[m, ~] = size(oldWaypoints) % get the size of the waypoints
 
-currentWaypoint = 1; % the current point
+currentWaypoint = 2 % the current point
 
 while currentWaypoint ~= m % while there are still more waypoints
     %pause(2);
-    intersectionFlag = 0; % this flag is to check for an intersection
+    intersectionFlag = 0 % this flag is to check for an intersection
     
-    checkingWaypoint = currentWaypoint + 1;
+    checkingWaypoint = currentWaypoint + 1
     
     while ~intersectionFlag && checkingWaypoint ~= m % while there is no intersection
         
         %check the line of sight for an obstacle
         
         %distance between waypoints
-        dist = norm(oldWaypoints(checkingWaypoint, 1:2) - oldWaypoints(currentWaypoint, 1:2));
+        dist = norm(oldWaypoints(checkingWaypoint, 1:2) - oldWaypoints(currentWaypoint, 1:2))
         %slope of x axis
         unitVector = (oldWaypoints(checkingWaypoint, 1:2) - oldWaypoints(currentWaypoint, 1:2)) / dist;
         
@@ -31,17 +31,17 @@ while currentWaypoint ~= m % while there are still more waypoints
             %check for occupancy
             if getOccupancy(map, oldWaypoints(currentWaypoint, 1:2) + (it * unitVector))
                 
-                intersectionFlag = 1; % set the flag high
+                intersectionFlag = 1 % set the flag high
                 
                 %update the Waypoints list if checkingWP - currentWP > 1
-                if checkingWaypoint - currentWaypoint > 1
+                if checkingWaypoint - currentWaypoint > 1 || currentWaypoint == 2
                     %added checkingWP - 1 to list and set it as the current point 
                     [rows, ~] = size(Waypoints);
                     Waypoints(rows + 1, 1:3) = oldWaypoints(checkingWaypoint - 1, 1:3);
-                    checkingWaypoint - 1;
+                    checkingWaypoint - 1
                     
                     %set new current point
-                    currentWaypoint = checkingWaypoint - 1;
+                    currentWaypoint = checkingWaypoint - 1
                     
                     break; % break it
                 end
